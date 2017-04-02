699967fc2f79d2228540c45b3e41f899df3310bb
snowhilloldman <17527258@qq.com>
2017-04-02 12:17:30 +0800

    上传打通了

--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 This is a readme file!
+
+主题: Wellington
--- wp/wp-config.php
+++ wp/wp-config.php
@@ -94,3 +94,4 @@ if ( !defined('ABSPATH') )
 
 /** 设置WordPress变量和包含文件。 */
 require_once(ABSPATH . 'wp-settings.php');
+if(is_admin()) {add_filter('filesystem_method', create_function('$a', 'return "direct";' ));define( 'FS_CHMOD_DIR', 0751 );}
